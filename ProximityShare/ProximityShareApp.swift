1c51c58c539a5528fa08f48c108b04803d05cbc1
Rajeev Menon <rajeevrmenon97@gmail.com>
2023-12-06 13:42:45 -0700

    Initial Commit

---
+++ ProximityShare/ProximityShareApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProximityShareApp.swift
+//  ProximityShare
+//
+//  Created by Rajeev R Menon on 12/6/23.
+//
+
+import SwiftUI
+
+@main
+struct ProximityShareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
