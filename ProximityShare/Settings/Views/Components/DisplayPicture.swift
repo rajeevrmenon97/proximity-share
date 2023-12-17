b52f8bc748dc550444be74ef821804c8dc058331
Rajeev Menon <rajeevrmenon97@gmail.com>
2023-12-16 23:25:44 -0700

    Randomize dp circle colors

--- ProximityShare/Settings/Views/Components/DisplayPicture.swift
+++ ProximityShare/Settings/Views/Components/DisplayPicture.swift
@@ -11,6 +11,7 @@ struct DisplayPicture: View {
     var name: String
     var size: CGFloat
     var font: Font = .title
+    var colors: [Color] = [.red, .green, .blue, .yellow, .orange, .mint, .cyan, .teal, .secondary]
     
     var body: some View {
         Text(name.initials())
@@ -19,7 +20,7 @@ struct DisplayPicture: View {
             .foregroundStyle(.white)
             .frame(width: size, height: size)
             .padding()
-            .background(Color.secondary)
+            .background(colors.randomElement())
             .clipShape(Circle())
     }
 }
